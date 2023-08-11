950c1ffabfd98bfb6d4a513e1875a153ac501bcc
Kamil Wronka <contact@kamilwronka.com>
2023-08-11 17:47:22 +0200

    added runtime to pages

---
+++ src/app/repertoire/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export const runtime = "edge";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      wtf mens
+    </main>
+  );
+}
